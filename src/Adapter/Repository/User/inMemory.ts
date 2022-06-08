28981b6faae3d20fed8a2ae56d933e05c1911da4
Thomas Bouasli <thomas.bouasli@gmail.com>
2022-06-08 18:44:00 -0300

    [test] For Create User

---
+++ src/Adapter/Repository/User/inMemory.ts
@@ -0,0 +1,20 @@
+import { User } from "@Domain/Entity";
+import { Either, Left, Right } from "@Util/FunctionalErrorHandler";
+import { IUserRepository } from "./interface";
+
+export class InMemoryUserRepository implements IUserRepository{
+    private users: User[] = [];
+
+    async findByEmail(email: string): Promise<User | undefined> {
+        return this.users.find(user => user.email === email);
+    }
+
+    async create(user: User): Promise<Either<Error, User>> {
+        const userExists = await this.findByEmail(user.email);
+        if (userExists) {
+            return new Left(new Error("User already exists"));
+        }
+        this.users.push(user);
+        return new Right(user);
+    }
+}
---
+++ src/Domain/UseCase/create-user/index.spec.ts
@@ -0,0 +1,11 @@
+import { InMemoryUserRepository } from "@Adapter/Repository/User/inMemory";
+import { CreateUser } from ".";
+
+const userRepo = new InMemoryUserRepository();
+const service = new CreateUser(userRepo);
+
+it("should create a new user", async () => {
+  const user = await service.execute({ name: "John Doe", email: "john@doe.com", password: "123456" });
+  expect(user.isRight()).toBeTruthy();
+  expect(user.value.name).toBe("John Doe");
+});
