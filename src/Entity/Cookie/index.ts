24f2a237772c136f76107bb9affcfcfcfb83f133
Thomas Bouasli <thomas.bouasli@gmail.com>
2022-06-16 17:04:40 -0300

    [refactor] Changed Owner pick to omit

--- src/Entity/Cookie/index.ts
+++ src/Entity/Cookie/index.ts
@@ -3,5 +3,5 @@ import { User } from 'Entity/User';
 export type Cookie = {
   id: string;
   name: string;
-  owner: Pick<User, 'id' | 'name' | 'email'>;
+  owner: Omit<User, 'cookies' | 'password'>;
 };
