b3dd30c3f8f4a070a0a553dca2f208a46c6b9be9
Thomas Bouasli <thomas.bouasli@gmail.com>
2022-06-23 21:16:34 -0300

    [refactor] Changed Default Styles

--- src/Application/Server/index.ts
+++ src/Application/Server/index.ts
@@ -1,40 +1,43 @@
 import Express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import { userRouter } from './Routes';
-import {
-  MockUserRepository,
-  PrismaCookieRepository,
-  PrismaUserRepository
-} from '@Adapter/Repository';
-import { MockCookieRepository } from '@Adapter/Repository';
+import { MockCookieRepository, MockUserRepository, PrismaCookieRepository, PrismaUserRepository } from '@Adapter/Repository';
 import path from 'path';
 
 const { NODE_ENV, PORT } = process.env;
-
 const app = Express();
-const userRepo =
-  NODE_ENV === 'test' ? new MockUserRepository() : new PrismaUserRepository();
-const cookieRepo =
-  NODE_ENV === 'test'
-    ? new MockCookieRepository(userRepo)
-    : new PrismaCookieRepository();
+
+// Setup Database
+
+const userRepo = NODE_ENV === 'test' ? new MockUserRepository() : new PrismaUserRepository();
+const cookieRepo = NODE_ENV === 'test' ? new MockCookieRepository(userRepo) : new PrismaCookieRepository();
+
+// Middlewares
 
 if (process.env.NODE_ENV === 'development') {
   app.use(cors());
 }
 app.use(Express.json());
+
+// Register Routes
+
 app.use('/api', userRouter(userRepo, cookieRepo));
+
+// Static Server
+
 app.use(Express.static(path.join(__dirname, '../../Adapter/view/build')));
 app.get('*', (req, res) => {
   //Let React Router handle all other routes
-  res.sendFile(
-    path.resolve(__dirname, '../../Adapter/view/build', 'index.html')
-  );
+  res.sendFile(path.resolve(__dirname, '../../Adapter/view/build', 'index.html'));
 });
+
+// Temporary Error Handler
+
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-  //Temporary Error Handler
   res.status(500).send({ message: err.message });
 });
+
+// Listen
 
 export const server = app.listen(PORT, () => {
   PORT !== '0' && console.log(`Server listening on port ${PORT}`);
